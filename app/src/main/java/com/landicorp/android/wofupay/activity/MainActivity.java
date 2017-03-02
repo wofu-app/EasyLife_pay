2460ad0bcb832c5ba4f3d867109d6d25f449e1f8
greatlv <191609732@qq.com>
2017-03-02 11:21:07 +0800

    第一次提交

--- app/src/main/java/com/landicorp/android/wofupay/activity/MainActivity.java
+++ app/src/main/java/com/landicorp/android/wofupay/activity/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Toast;
 import com.landicorp.android.wofupay.R;
 import com.landicorp.android.wofupay.base.BaseActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends BaseActivity {
     private Context context = this;
 
@@ -15,6 +17,14 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initData();
+    }
+
+    private void initData() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add("a"+i);
+        }
     }
 
     private void initView() {
